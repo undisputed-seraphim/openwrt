f3d10148f97020f7dcadd663abe88f006ef0cd70
DropDemBits <r3usrlnd@gmail.com>
2023-10-01 17:43:05 -0400

    ipq40xx: tp-link deco-m5: Use zImage format
    gzip compression isn't enough to generate factory format images,
    so use the zImage format which compresses the kernel more.
    
    Signed-off-by: Bee Cadorna <r3usrlnd@gmail.com>

--- target/linux/ipq40xx/image/generic-tp-link.mk
+++ target/linux/ipq40xx/image/generic-tp-link.mk
@@ -1,19 +1,19 @@
 DEVICE_VARS += TPLINK_BOARD_ID
 
-define Device/tplink-gzip
+define Device/tplink-zImage
 	DEVICE_VENDOR := TP-Link
 	IMAGES += factory.bin
 	IMAGE/factory.bin := append-rootfs | tplink-safeloader factory
 	IMAGE/sysupgrade.bin := append-rootfs | tplink-safeloader sysupgrade | append-metadata
-	KERNEL_SUFFIX := -uImage.itb
-	KERNEL = kernel-bin | gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
-	KERNEL_NAME := Image
+	KERNEL_SUFFIX := -zImage.itb
+	KERNEL = kernel-bin | fit none $(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := zImage
 	SOC := qcom-ipq4019
 	TPLINK_BOARD_ID :=
 endef
 
 define Device/tplink_deco-m5
-	$(call Device/tplink-gzip)
+	$(call Device/tplink-zImage)
 	DEVICE_MODEL := Deco-M5
 	TPLINK_BOARD_ID := DECO-M5
 	IMAGE_SIZE := 16640k
@@ -21,7 +21,7 @@ endef
 TARGET_DEVICES += tplink_deco-m5
 
 define Device/tplink_deco-m5-v2
-	$(call Device/tplink-gzip)
+	$(call Device/tplink-zImage)
 	DEVICE_MODEL := Deco-M5
 	DEVICE_VARIANT := v2
 	TPLINK_BOARD_ID := DECO-M5
