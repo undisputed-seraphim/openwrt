8d18debc2ed5e22f99d6fb29f2aa41ceffb33dbb
Tan Li Boon <undisputed.seraphim@gmail.com>
2025-06-22 14:31:50 +0800

    realtek: add platform support for RTL8198C SoCs
    Add target so we can compile for RTL8198C.
    The RTL8198C is based on a dual core MIPS 1074Kc.
    
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>

---
+++ target/linux/realtek/rtl8198c/target.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-only
+ARCH:=mips
+SUBTARGET:=rtl8198c
+CPU_TYPE:=24kc
+BOARD:=realtek
+BOARDNAME:=Realtek MIPS RTL8198C
+
+define Target/Description
+	Build firmware images for Realtek RTL8198C based boards.
+endef
